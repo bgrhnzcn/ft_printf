7bceb67eafc08e33fd71256d9a76d8b2ed9c8e4e
bgrhnzcn <bugrahanozcan16@gmail.com>
2023-11-19 00:57:19 +0300

    Tried to solve paco --strict KO's but can't test in Ubuntu machine.

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -3,19 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: buozcan <buozcan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 14:36:22 by buozcan           #+#    #+#             */
-/*   Updated: 2023/11/17 22:59:32 by buozcan          ###   ########.fr       */
+/*   Updated: 2023/11/19 00:56:55 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	ft_shorter_char(int *digit, long nb)
+{
+	int	check;
+
+	check = ft_putnbr(nb);
+	if (check == -1)
+		return (-1);
+	*digit += check;
+	return (0);
+}
+
+static int	ft_shorter_num(int *digit, long nb)
+{
+	int	check;
+
+	check = ft_putnbr(nb / 10);
+	if (check == -1)
+		return (-1);
+	*digit += check;
+	return (0);
+}
+
 int	ft_putnbr(long nb)
 {
 	int	digit;
-	int	check;
 
 	digit = 0;
 	if (nb < 0)
@@ -24,29 +45,19 @@ int	ft_putnbr(long nb)
 			return (-1);
 		digit += 1;
 		nb = -nb;
-		check = ft_putnbr(nb);
-		if (check == -1)
+		if (ft_shorter_char(&digit, nb))
 			return (-1);
-		digit += check;
 	}
 	else if (nb > 9)
 	{
-		check = ft_putnbr(nb / 10);
-		if (check == -1)
+		if (ft_shorter_num(&digit, nb))
 			return (-1);
-		digit += check;
 		nb = nb % 10;
-		check = ft_putchar(48 + nb);
-		if (check == -1)
+		if (ft_shorter_char(&digit, nb))
 			return (-1);
-		digit += check;
 	}
 	else
-	{
-		check = ft_putchar(48 + nb);
-		if (check == -1)
+		if (ft_shorter_char(&digit, nb))
 			return (-1);
-		digit += check;
-	}
 	return (digit);
 }
